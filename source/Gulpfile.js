43ab19d671ac0e58ddf7e5963e7f4deeef7d6358
ntlk <hi@ntlk.net>
2016-05-26 16:39:34 +0100

    Add the accordion file to Gulpfile

--- source/Gulpfile.js
+++ source/Gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var watch = require('gulp-watch');
 var concat = require('gulp-concat');
 
 gulp.task('scripts', function() {
-  return gulp.src(['./scripts/underscore.js', 'scripts/jquery.js', 'scripts/backbone.js', 'scripts/navigation.js', 'scripts/popup.js', 'scripts/bar.js', 'scripts/screen.js', 'scripts/router.js', 'scripts/app.js'])
+  return gulp.src(['./scripts/underscore.js', 'scripts/jquery.js', 'scripts/backbone.js', 'scripts/navigation.js', 'scripts/accordion.js', 'scripts/popup.js', 'scripts/bar.js', 'scripts/screen.js', 'scripts/router.js', 'scripts/app.js'])
     .pipe(concat('prototyping-kit.js'))
     .pipe(gulp.dest('./../'))
     .pipe(gulp.dest('./../example/assets/js/'));
